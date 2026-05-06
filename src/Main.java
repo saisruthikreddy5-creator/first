478fc1e4dfd7f274354d04999f32a48e5c8ae722
Sai sruthik <saisruthikreddy5@gmail.com>
2026-05-06 14:30:07 +0530

    C:\Users\LENOVO\IdeaProjects\first

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import Statements.statements;
+import classsharing.classA;
 import operator.operators;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-loops o= new loops();
-o.perfectnumber(6);
+classA o= new classA();
+System.out.println(o.getA());
     }
 }
---
+++ src/classsharing/classA.java
@@ -0,0 +1,8 @@
+package classsharing;
+
+public class classA {
+    public int getA(){
+      return new classB().getb();
+    }
+
+}
